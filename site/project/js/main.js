1b7584a4f86c3aa2be55b0dd7701c3be2e520fed
Amirkhan Salan <68302923+n7wt@users.noreply.github.com>
2023-01-21 00:48:38 +0300

    crypto-project
    practice

---
+++ site/project/js/main.js
@@ -0,0 +1,14 @@
+// Вывод текста через alert
+function Tuned() {
+  alert("GAME WILL BE SOON...");
+}
+
+// "Правильные" переменные
+let x = 25,
+  y = 8;
+console.log(x - y);
+
+// "Неправильные" переменные
+// let 1Chislo = 30, 2Chislo = 25;
+// console.log (1chislo - 2chislo);
+// Закомментировал, чтобы alert выше работал ^
